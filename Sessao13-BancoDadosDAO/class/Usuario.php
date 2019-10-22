a214f00e025d8bf893b30104a2bcf6e80a7f8372
David Kenny <contato@webproinfo.com>
2019-10-22 12:12:01 -0300

    Atualizado 22/10/19 12:11

--- Sessao13-BancoDadosDAO/class/Usuario.php
+++ Sessao13-BancoDadosDAO/class/Usuario.php
@@ -52,6 +52,69 @@ class Usuario {
 	}
 	//Término: Método select por id
 
+
+
+	//Inicio: Método select por usuário
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",
+			array(
+				':SEARCH'=>"%".$login."%"
+			));
+
+	}
+	//Término: Método select por usuário
+
+
+	//Início: Método validação de Login
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (isset($result[0])) {
+
+			$row = $result[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else {
+
+			throw new Exception("Login e/ou senha invalidos.");
+			
+
+		}
+
+	}
+	//Término: Método validação de Login
+
+
+
+	//Início: Método para selecionar todos usuários da tabela
+	public static function getList(){
+
+	//Método static porque não utilizo o $this que faz ref a variaveis
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+
+	}
+	//Término: Método para selecionar todos usuários da tabela
+
+
+
 	//Início: Método toString
 	public function __toString(){
 
--- Sessao13-BancoDadosDAO/index.php
+++ Sessao13-BancoDadosDAO/index.php
@@ -2,10 +2,35 @@
 
 require_once("config.php");
 
+
+/*
+--------Retorna um usuário pelo id dele--------
 $usuario = new Usuario();
-
 $usuario->loadById(4);
+echo $usuario;
+*/
 
+/*
+--------Retorna uma lista de usuários--------
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+
+/*
+--------Carrega uma lista de usuários buscando pelo login--------
+
+$search = Usuario::search("vid");
+echo json_encode($search);
+*/
+
+/*
+--------Carregar usuário utilizando login e senha--------
+*/
+
+$usuario = new Usuario();
+$usuario->login("david","45");
 echo $usuario;
 
  ?>
